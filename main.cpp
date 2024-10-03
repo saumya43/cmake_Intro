26461cc006d41489a44d2cf1c3987bd548ab1e48
saumya43 <saumya043@example.com>
2024-10-03 05:47:59 +0000

    cmake introduction

---
+++ main.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+int main() { std::cout << "hey, nice to meet you\n"; return 0; }
---
+++ out/build/CMakeFiles/ubuntu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ubuntu.dir/main.cpp.o"
+  "CMakeFiles/ubuntu.dir/main.cpp.o.d"
+  "ubuntu"
+  "ubuntu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ubuntu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
